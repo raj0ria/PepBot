91c2ea4a718d97ab9171bbff246f8d20bc9f2b61
raj0ria <ashish13102376@gmail.com>
2024-04-11 15:47:38 +0000

    Added the chat bot ui and connected to the openai api

--- pepbot-web/src/app/page.js
+++ pepbot-web/src/app/page.js
@@ -1,63 +1,120 @@
 'use client'
 
 import { useClient } from 'next/client'; 
-import { useState } from "react"
+import { useState } from "react";
+import Head from "next/head";
+import ReactMarkdown from "react-markdown";
 
 const SYSTEM_MESSAGE = "You are PepBot, An versatile and helpful AI created by pepcus using state of the art ML models and API's";
 
 export default function Home() {
   const [apiKey, setApiKey] = useState('');
-  const [botMessage, setBotMessage] = useState('');
+  const [userMessage, setUserMessage] = useState('');
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content:
+        "You are Pepbot, a helpful AI developed by Pepcus and powered by state-of-the-art machine learning models.",
+    },
+  ]);
   
   const API_URL = "https://api.openai.com/v1/chat/completions"
 
-  async function sendRequest() {
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer " + apiKey,
+  const sendRequest = async () => {
+
+    const updatedMessages = [
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
       },
-      body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-       "messages": [
-           { "role": "system", "content": SYSTEM_MESSAGE },
-           { "role": "user", "content": "Hello, Please introduce yourself!" },
-        ]
-      })
+    ];
 
-    })
+    setMessages(updatedMessages);
+    setUserMessage("");
 
-    const responseJson = await response.json()
-    setBotMessage(responseJson.choices[0].message.content)
-    console.log("response.json", responseJson)
-  }
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+        }),
+      });
+
+      const resJson = await response.json();
+      console.log(resJson);
+
+      const updatedMessages2 = [...updatedMessages, resJson.choices[0].message];
+      setMessages(updatedMessages2);
+    } catch (error) {
+      console.error("error");
+      window.alert("Error:" + error.message);
+    }
+  };
 
   return (
-   <div className="flex flex-col h-screen" >
-    <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-      <div className="text-xl font-bold">PepBot</div>
-      <div>
-        <input 
-        type="password"
-        className = "border p-1 rounded"
-        onChange={e => setApiKey(e.target.value)}
-        value={apiKey}
-        placeholder="Paste your api key over here" />
+    <>
+      <Head>
+        <title>Jobot</title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navbar */}
+        <nav className="bg-white shadow w-full">
+          <div className="px-4 h-14 flex justify-between items-center">
+            <div className="text-xl font-bold">Jobot</div>
+            <div>
+              <input
+                type="password"
+                className="border rounded p-1"
+                placeholder="Enter API key.."
+                value={apiKey}
+                onChange={(e) => setApiKey(e.target.value)}
+              />
+            </div>
+          </div>
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll">
+          <div className="mx-auto w-full max-w-screen-md p-4 ">
+            {messages
+              .filter((msg) => msg.role !== "system")
+              .map((msg, idx) => (
+                  <div key={idx} className="mt-3">
+                    <div className="font-bold">
+                      {msg.role === "user" ? "You" : "Jobot"}
+                    </div>
+                    <div className="prose-lg">
+                      <ReactMarkdown>{msg.content}</ReactMarkdown>
+                    </div>
+                  </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Message Input */}
+        <div className="mx-auto w-full max-w-screen-md px-4 pt-0 pb-2 flex">
+          <textarea
+            className="border rounded-md text-lg p-2 flex-1"
+            rows={1}
+            placeholder="Ask me anything..."
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+          />
+          <button
+            onClick={sendRequest}
+            className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white px-4 ml-2"
+          >
+            Send
+          </button>
+        </div>
       </div>
-    </nav>
-
-    <div className="p-4">
-      <button className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600"
-      onClick={sendRequest}>
-        Send Request
-      </button>
-      <div className='text-lg mt-4'>{botMessage}</div>
-
-    </div>
-
-   </div>
-
-  )
+    </>
+  );
 }
 
